f03eb8d493fc883115bc2d60b8d32a6826437423
Tom Titcombe <t.j.titcombe@gmail.com>
2020-06-29 23:06:05 +0100

    Move pruning to a separate function

--- main.py
+++ main.py
@@ -8,6 +8,35 @@ from tqdm import trange
 from src import LeNet, mnist_train_transform
 
 
+def prune_model(model, prune_pc):
+    # TODO generalise this
+    parameters_to_prune = (
+        (model.features.first_conv, 'weight'),
+        (model.features.second_conv, 'weight'),
+        (model.classifier.first_linear, 'weight'),
+        (model.classifier.second_linear, 'weight'),
+        (model.classifier.third_linear, 'weight'),
+    )
+
+    # Prune model
+    prune.global_unstructured(
+        parameters_to_prune,
+        pruning_method=prune.L1Unstructured,
+        amount=prune_pc_per_round,
+    )
+
+    # TODO generalise this process
+    # Number of *unpruned* parameters
+    pruned_parameters = float(
+            torch.sum(model.features.first_conv.weight != 0)
+            + torch.sum(model.features.second_conv.weight != 0)
+            + torch.sum(model.classifier.first_linear.weight != 0)
+            + torch.sum(model.classifier.second_linear.weight != 0)
+            + torch.sum(model.classifier.third_linear.weight != 0)
+        )
+
+    return model, pruned_parameters
+
 def main(args):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -23,8 +52,17 @@ def main(args):
 
     model.train()
 
+    # Number of parameters in total (pruned and unpruned)
+    total_parameters = float(
+            model.features.first_conv.weight.nelement()
+            + model.features.second_conv.weight.nelement()
+            + model.classifier.first_linear.weight.nelement()
+            + model.classifier.second_linear.weight.nelement()
+            + model.classifier.third_linear.weight.nelement()
+        )
+
     # Percentage to *prune* per round
-    prune_pc_per_round = 1 - (1 - args.prune_pc) ** (1 / args.prune_rounds)
+    prune_pc_per_round = 1 - (1 - args.prune_pc) ** (1/args.prune_rounds)
 
     for round in range(args.prune_rounds):
         for epoch in trange(args.epochs):
@@ -49,42 +87,10 @@ def main(args):
 
             epoch_acc = 100 * total / correct
 
-        # TODO generalise this
-        parameters_to_prune = (
-            (model.features.first_conv, "weight"),
-            (model.features.second_conv, "weight"),
-            (model.classifier.first_linear, "weight"),
-            (model.classifier.second_linear, "weight"),
-            (model.classifier.third_linear, "weight"),
-        )
-
         # Prune model
-        prune.global_unstructured(
-            parameters_to_prune,
-            pruning_method=prune.L1Unstructured,
-            amount=prune_pc_per_round,
-        )
+        model, pruned_parameters = prune_model(model, prune_pc_per_round)
 
         # TODO re-initialise lottery ticket weights
-
-        # TODO generalise this process
-        # Number of *unpruned* parameters
-        pruned_parameters = float(
-            torch.sum(model.features.first_conv.weight != 0)
-            + torch.sum(model.features.second_conv.weight != 0)
-            + torch.sum(model.classifier.first_linear.weight != 0)
-            + torch.sum(model.classifier.second_linear.weight != 0)
-            + torch.sum(model.classifier.third_linear.weight != 0)
-        )
-
-        # Number of parameters in total (pruned and unpruned)
-        total_parameters = float(
-            model.features.first_conv.weight.nelement()
-            + model.features.second_conv.weight.nelement()
-            + model.classifier.first_linear.weight.nelement()
-            + model.classifier.second_linear.weight.nelement()
-            + model.classifier.third_linear.weight.nelement()
-        )
 
         print(f"Final accuracy: {epoch_acc:.3f}")
         print(f"Model parameters: {pruned_parameters}")
@@ -100,18 +106,8 @@ if __name__ == "__main__":
     parser.add_argument(
         "--epochs", default=8, type=int, help="Number of epochs (default 8)"
     )
-    parser.add_argument(
-        "--prune_pc",
-        default=0.2,
-        type=float,
-        help="Percentage of parameters to prune over the course of the training process (default 0.2)",
-    )
-    parser.add_argument(
-        "--prune_rounds",
-        default=5,
-        type=int,
-        help="Number of rounds of pruning to perform (default 5)",
-    )
+    parser.add_argument("--prune_pc", default=0.2, type=float, help="Percentage of parameters to prune over the course of the training process (default 0.2)")
+    parser.add_argument("--prune_rounds", default=5, type=int, help="Number of rounds of pruning to perform (default 5)")
 
     args = parser.parse_args()
 
